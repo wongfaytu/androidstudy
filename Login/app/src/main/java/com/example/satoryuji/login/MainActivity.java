70ca3751b34f89d06f573706a760d0d7e84877c7
wongfaytu <>
2018-08-11 20:30:09 +0900

    一つ目のフラグメントの作成とボタンなどの追加

--- Login/app/src/main/java/com/example/satoryuji/login/MainActivity.java
+++ Login/app/src/main/java/com/example/satoryuji/login/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.satoryuji.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainFragment fragment = new MainFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.activity_main, fragment);
+        transaction.commit();
     }
 }
