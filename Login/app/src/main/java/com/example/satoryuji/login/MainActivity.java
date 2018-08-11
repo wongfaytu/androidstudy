0411555640b3f9d9c4b9aef11f57e357d4d2cd90
wongfaytu <>
2018-08-12 00:11:48 +0900

    no message

--- Login/app/src/main/java/com/example/satoryuji/login/MainActivity.java
+++ Login/app/src/main/java/com/example/satoryuji/login/MainActivity.java
@@ -1,20 +1,2 @@
 package com.example.satoryuji.login;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        MainFragment fragment = new MainFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.activity_main, fragment);
-        transaction.commit();
-    }
-}
--- Login/app/src/main/java/com/example/satoryuji/login/MainFragment.java
+++ Login/app/src/main/java/com/example/satoryuji/login/MainFragment.java
@@ -1,31 +1,4 @@
 package com.example.satoryuji.login;
 
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
-public class MainFragment extends Fragment {
-    @Override
-    public View onCreateView(LayoutIonflater inflater, ViewGroup activity_main, Bundle savedInstanceState) {
-        super.onCreateView(inflater, activity_main, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_main, activity_main, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        Button button = (Button) view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
-    }
-}
