64240df29d3cdcdb330800cf3d01a8f88778d4d5
wongfaytu <>
2018-08-12 01:04:50 +0900

    やり直しデータ付きのフラグメント遷移

--- MyApplication/app/src/main/java/com/example/satoryuji/study/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/satoryuji/study/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.satoryuji.study;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+ import android.support.v4.app.FragmentTransaction;
+ import android.support.v7.app.AppCompatActivity;
+
+   
 
 public class MainActivity extends AppCompatActivity {
+      
+    @Override  
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_main);  
+        MainFragment fragment = new MainFragment(); 
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction(); 
+        transaction.replace(R.id.activity_main, fragment); 
+        transaction.commit(); 
     }
+
+     
 }
+
