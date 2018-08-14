7f91f6b580cc365fafa39211c25cef044b6cb57d
wongfaytu <>
2018-08-14 16:46:31 +0900

    諸々のやり直しとエラーが起きて実行ができない

--- MyApplication/app/src/main/java/com/example/satoryuji/study/MainFragment.java
+++
@@ -1,38 +0,0 @@
-package com.example.satoryuji.study;
-
-import android.os.Bundle;
- import android.support.annotation.Nullable;
- import android.support.v4.app.Fragment; 
-import android.support.v4.app.FragmentManager; 
-import android.view.View;
- import android.view.ViewGroup;
- import android.widget.Button;
-
-  
-
-public class MainFragment extends Fragment {
-     
-    @Override  
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup activity_main, Bundle savedInstanceState) { 
-        super.onCreateView(inflater, activity_main, savedInstanceState);
-         return inflater.inflate(R.layout.fragment_main, activity_main, false);
-         }
-
-      
-    @Override  
-
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) { 
-        super.onViewCreated(view, savedInstanceState);
-          Button button = (Button) view.findViewById(R.id.button);
-         button.setOnClickListener(new View.OnClickListener() {
-             
-            @Override 
-
-            public void onClick(View v) { 
-                 }
-
-             
-        });
-         }
-         }
